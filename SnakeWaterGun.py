275312b2c00c7463880afc9ae0d052cee42917d8
schauhan2459 <130307427+schauhan2459@users.noreply.github.com>
2023-08-27 12:54:05 +0530

    Update SnakeWaterGun.py

--- SnakeWaterGun.py
+++ SnakeWaterGun.py
@@ -1,72 +1,92 @@
 #Snake Water Gun
 
 import random
+import tkinter as tk
+from tkinter import messagebox
 
-def game():
-    n=int(input("\nEnter no. of round you want to play: "))
-    p=0
-    c=0
-    t=0
+class SnakeWaterGunGame:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Snake Water Gun Game")
 
-    while n>t:
-        choice=["Snake","Water","Gun"]
-        print("\n----------------------------------\n")
-        print(F"Available roles are:")
-        print(f"1. {choice[0]}  2. {choice[1]}  3. {choice[2]}")
-        r=str(input("Enter your choice: "))
-        cp=random.choice(choice)
-        print(f"Computer Choice: {cp}")
-        
-        if r==cp:
-            print("Draw")
-            c=c+1
-            p=p+1
-            print(f"you:{p} computer:{c}\n")
-        elif r=="Snake" and cp=="Water":
-            print("You won")
-            c=c
-            p=p+1
-            print(f"you:{p} computer:{c}\n")
-        elif r=="Snake" and cp=="Gun":
-            print("You lose")
-            c=c+1
-            p=p
-            print(f"you:{p} computer:{c}\n")
-        elif r=="Water" and cp=="Snake":
-            print("You lose")
-            c=c+1
-            p=p
-            print(f"you:{p} computer:{c}\n")
-        elif r=="Water" and cp=="Gun":
-            print("You Won")
-            c=c
-            p=p+1
-            print(f"you:{p} computer:{c}\n")
-        elif r=="Gun" and cp=="Snake":
-            print("\nYou Won")
-            c=c
-            p=p+1
-            print(f"you:{p} computer:{c}\n")
-        elif r=="Gun" and cp=="Water":
-            print("You lose")
-            c=c+1
-            p=p
-            print(f"you:{p} computer:{c}\n")
+        self.user_choices = ["snake", "water", "gun"]
+        self.user_score = 0
+        self.computer_score = 0
+        self.round_num = 1
+
+        self.label = tk.Label(root, text="Snake Water Gun Game", font=("Helvetica", 16))
+        self.label.pack(pady=10)
+
+        self.round_label = tk.Label(root, text=f"Round {self.round_num}", font=("Helvetica", 12))
+        self.round_label.pack()
+
+        self.choice_label = tk.Label(root, text="Choose snake, water, or gun:")
+        self.choice_label.pack()
+
+        self.choice_var = tk.StringVar(root)
+        self.choice_var.set(self.user_choices[0])
+        self.choice_menu = tk.OptionMenu(root, self.choice_var, *self.user_choices)
+        self.choice_menu.pack()
+
+        self.result_label = tk.Label(root, text="", font=("Helvetica", 12))
+        self.result_label.pack()
+
+        self.score_label = tk.Label(root, text="Score:", font=("Helvetica", 12))
+        self.score_label.pack()
+
+        self.score_display = tk.Label(root, text="You: 0 | Computer: 0")
+        self.score_display.pack()
+
+        self.play_button = tk.Button(root, text="Play", command=self.play_round)
+        self.play_button.pack(pady=10)
+
+    def play_round(self):
+        user_choice = self.choice_var.get()
+        computer_choice = random.choice(self.user_choices)
+
+        result = self.check_result(user_choice, computer_choice)
+        self.update_scores(result)
+        self.update_ui(user_choice, computer_choice, result)
+
+        self.round_num += 1
+        self.round_label.config(text=f"Round {self.round_num}")
+
+    def check_result(self, user_choice, computer_choice):
+        if user_choice == computer_choice:
+            return "draw"
+        elif (user_choice == "snake" and computer_choice == "water") or \
+             (user_choice == "water" and computer_choice == "gun") or \
+             (user_choice == "gun" and computer_choice == "snake"):
+            return "win"
         else:
-            print("Invalid Choice.Try Again!")
-            break
-        t=t+1
-    
-    print("----------------------------------\n")
-    if p<c:
-        print("Final : Computer Win!!")
-        print(f"You:{p} Computer:{c}")
-    elif p==c:
-        print("Final : Draw")
-        print(f"You:{p} Computer:{c}")
-    else:
-        print("Final : You Win!!")
-        print(f"You:{p} Computer:{c}")
-    print("\n----------------------------------")
-        
-game()
+            return "lose"
+
+    def update_scores(self, result):
+        if result == "win":
+            self.user_score += 1
+        elif result == "lose":
+            self.computer_score += 1
+
+    def update_ui(self, user_choice, computer_choice, result):
+        self.result_label.config(text=f"Computer chose {computer_choice}. You {result}!")
+        self.score_display.config(text=f"You: {self.user_score} | Computer: {self.computer_score}")
+
+        if self.round_num > 5:
+            self.show_game_over()
+
+    def show_game_over(self):
+        game_over_message = "Game Over!\n\n"
+        if self.user_score > self.computer_score:
+            game_over_message += "Congratulations! You win!"
+        elif self.user_score < self.computer_score:
+            game_over_message += "Computer wins the game. Better luck next time!"
+        else:
+            game_over_message += "It's a tie!"
+
+        messagebox.showinfo("Game Over", game_over_message)
+        self.root.destroy()
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = SnakeWaterGunGame(root)
+    root.mainloop()
